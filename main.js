69417a69f549f6e88ff8fb69c73b6f57f364263f
kamehameha <76761719+kamehama@users.noreply.github.com>
2021-05-05 18:10:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -28,14 +28,11 @@ var last_position_of_x, last_position_of_y;
         ctx.strokeStyle = color;
         ctx.lineWidth = width_of_line;
 
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
+      
 
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
+       
+         ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, 40, 0,2*Math.PI)
+            ctx.stroke();
         }
 
         last_position_of_x = current_position_of_mouse_x; 
